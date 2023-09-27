9f467a948517efc3f74b104f1f8fbf5f3e99f460
Azizjon <131476746+AziKo-D-e-V@users.noreply.github.com>
2023-09-27 17:42:44 +0500

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 const config = {
-  TOKEN: "6234421205:AAERrdoK5AfKv41i9Vq76o9X-K4Z0m4oNKs",
-  DB_URL:
-    "mongodb+srv://azizjondev:yQYUp3ZWjOWMkePl@cluster0.t90czyz.mongodb.net/Lotin-Krill-Bot?retryWrites=true&w=majority",
+  TOKEN: process.env.Token,
+  DB_URL: process.env.DB_URL,
 };
 
 module.exports = config;
